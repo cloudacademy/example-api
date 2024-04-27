44e5dc471efa966ea1492159ce07ac2e18cf58b5
lrakai <lmrakai@gmail.com>
2024-04-26 23:09:22 -0600

    include application token validation

--- apigateway/main.py
+++ apigateway/main.py
@@ -1,14 +1,30 @@
 from flask import Flask, request, make_response
 from flask_cors import CORS
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/", methods=["OPTIONS"])
+def preflight():
+    # Return a 200 response with CORS headers set for preflight OPTIONS requests
+    return _set_cors_headers(make_response())
 
-@app.route("/", methods=["GET","OPTIONS"])
+@app.route("/", methods=["GET"])
 def hello():
+    # Verify the token from access_token url parameter
+    id_info = id_token.verify_oauth2_token(
+        request.args.get("access_token"),
+        requests.Request(),
+        "Replace Client ID"
+    )
 
-    resp = make_response("Welcome CA")
+    # Return a message including the name claim of the token
+    resp = make_response(f"Welcome {id_info['name']}")
+    return _set_cors_headers(resp)
+
+def _set_cors_headers(resp):
     resp.access_control_allow_credentials = True
     resp.headers["Access-Control-Allow-Origin"] = request.origin
     resp.headers["Access-Control-Allow-Methods"] = "GET"
